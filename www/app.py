bbfb8274b0f0560f437d7613a644e64aa3241477
ss <13122231591@163.com>
2019-04-19 12:36:38 +0800

    app.py 15 .->,

--- www/app.py
+++ www/app.py
@@ -12,7 +12,7 @@ def index(request):
 async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_router('GET','/',index)
-    srv = await loop.create_server(app.make_handler(),'127.0.0.1'.9000)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info("server started at http://127.0.0.1:9000...")
     return srv
 
