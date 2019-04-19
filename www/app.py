f59b636775c7dd4944ec656b568f3befe40515ed
ss <13122231591@163.com>
2019-04-19 16:21:55 +0800

    app.py print 9000->11000

--- www/app.py
+++ www/app.py
@@ -16,7 +16,7 @@ async def init(loop):
     app = web.Application(loop=loop)
     app.add_routes(routes)
     srv = await loop.create_server(app.make_handler(),'127.0.0.1',11000)
-    logging.info("server started at http://127.0.0.1:9000...")
+    logging.info("server started at http://127.0.0.1:11000...")
     return srv
 
 loop = asyncio.get_event_loop()
