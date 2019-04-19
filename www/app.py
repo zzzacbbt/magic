68a73f60440e96378d97077eaa2fcedcc223a671
ss <13122231591@163.com>
2019-04-19 15:43:52 +0800

    app.py 9000->11000

--- www/app.py
+++ www/app.py
@@ -6,16 +6,21 @@ from datetime import datetime
 
 from aiohttp import web
 
+routes = web.RouteTableDef()
+
+@routes.get('/')
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
 
 async def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_route('GET','/',index)
-    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    app.add_routes(routes)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1',11000)
     logging.info("server started at http://127.0.0.1:9000...")
     return srv
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+
